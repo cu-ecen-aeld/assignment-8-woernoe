84a754ae65026fb570b10e495c49c03f79eb854e
woernoe <we@ernstware.de>
2025-11-21 13:31:36 +0100

    Assignment 8 V01

---
+++ base_external/package/aesd-chardriver/aesd-chardriver.mk
@@ -0,0 +1,45 @@
+
+##############################################################
+#
+# AESD-CHARDRIVER
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+#AESD_CHARDRIVER_VERSION = '#COMMIT VERSION NUMBER'
+AESD_CHARDRIVER_VERSION = 3cba4e058188ff43086b4d620b53ba2cf3ff670d
+
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+#AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+AESD_CHARDRIVER_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-woernoe.git
+AESD_CHARDRIVER_SITE_METHOD = git
+AESD_CHARDRIVER_GIT_SUBMODULES = YES
+
+AESD_CHARDRIVER_LICENCE = GPL-2.0
+AESD_CHARDRIVER_DEPENDENCIES = linux
+
+
+AESD_CHARDRIVER_MODULE_SUBDIRS = aesd-char-driver
+
+define AESD_CHARDRIVER_BUILD_CMDS
+
+       $(MAKE)  -C $(LINUX_DIR) \
+                 M=$(@D) \
+                 MODULE_SUBDIRS="$(addprefix $(@D)/, $(AESD_CHARDRIVER_MODULE_SUBDIRS))" \
+                 $(LINUX_MAKE_FLAGS) modules
+endef
+
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+define AESD_CHARDRIVER_INSTALL_TARGET_CMDS
+        $(MAKE) -C $(LINUX_DIR) \
+                 M=$(@D) \
+                 MODULE_SUBDIRS="$(addprefix $(@D)/, $(AESD_CHARDRIVER_MODULE_SUBDIRS))" \
+                 $(LINUX_MAKE_FLAGS)  \
+                 INSTALL_MOD_PATH=$(TARGET_DIR) modules_install
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
+
